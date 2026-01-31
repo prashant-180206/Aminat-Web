45204d9a9d6f7cdfcfa2728071c887a250d8f74d
PRASHANT RAJESH SUTHAR <prashant.suthar24@vit.edu>
2026-02-01 00:10:00 +0530

    Project Completed

--- remotion/compositions/index.ts
+++
@@ -1,7 +0,0 @@
-export { ProfessionalIntro } from "./ProfessionalIntro";
-export { TitleSequence } from "./TitleSequence";
-export { AnimatedStatsVideo } from "./AnimatedStatsVideo";
-export { StatsShowcase } from "./StatsShowcase";
-export { FeatureShowcase } from "./FeatureShowcase";
-export { TestimonialShowcase } from "./TestimonialShowcase";
-export { ProcessShowcase } from "./ProcessShowcase";
